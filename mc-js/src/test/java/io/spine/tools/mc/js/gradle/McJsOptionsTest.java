4837828be8ee8a69ca8a359ccf4ba542fe95f672
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2021-12-02 20:09:54 +0200

    Improve test data

--- mc-js/src/test/java/io/spine/tools/mc/js/gradle/McJsOptionsTest.java
+++ mc-js/src/test/java/io/spine/tools/mc/js/gradle/McJsOptionsTest.java
@@ -26,6 +26,8 @@
 
 package io.spine.tools.mc.js.gradle;
 
+import com.google.common.collect.ImmutableList;
+import io.spine.tools.fs.DirectoryPattern;
 import io.spine.tools.fs.ExternalModule;
 import io.spine.tools.fs.ExternalModules;
 import org.gradle.api.Project;
@@ -39,14 +41,14 @@ import org.junit.jupiter.api.io.TempDir;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.google.common.truth.Truth.assertThat;
-import static io.spine.tools.mc.js.gradle.McJsOptions.in;
-import static java.util.Collections.emptyList;
 
 @DisplayName("`McJsOptions` should")
 class McJsOptionsTest {
 
+    // As defined by `resources/META-INF/gradle-plugins/io.spine.mc-js.properties`.
     private static final String PLUGIN_ID = "io.spine.mc-js";
 
     private static final String GROUP_ID = "my.company";
@@ -70,15 +72,23 @@ class McJsOptionsTest {
     @Test
     @DisplayName("add custom modules to resolve")
     void setModulesToResolve() {
-        String moduleName = "foo-bar";
-        Map<String, List<String>> modulesExt = pluginExtension().modules;
-        modulesExt.put(moduleName, emptyList());
-        ExternalModules modules = pluginExtension().combinedModules();
-        assertThat(modules.asList())
-                .contains(new ExternalModule(moduleName, emptyList()));
-    }
+        String moduleName = "foo-bar-module";
+        McJsOptions options = McJsOptions.in(project);
+        Map<String, List<String>> modules = options.modules;
+        List<String> customModules = ImmutableList.of(
+                "foo",
+                "bar",
+                "baz"
+        );
+        modules.put(moduleName, customModules);
 
-    private McJsOptions pluginExtension() {
-        return in(project);
+        ExternalModules combinedModules = options.combinedModules();
+
+        List<DirectoryPattern> patterns = customModules.stream()
+                .map(DirectoryPattern::of)
+                .collect(Collectors.toList());
+        ExternalModule expected = new ExternalModule(moduleName, patterns);
+        assertThat(combinedModules.asList())
+                .contains(expected);
     }
 }
