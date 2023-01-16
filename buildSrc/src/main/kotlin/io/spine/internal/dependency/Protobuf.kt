3107871df5f5467511d5dddd90372652ff1c5e88
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-16 11:38:18 +0000

    Fix comment layout

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Protobuf.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Protobuf.kt
@@ -30,8 +30,8 @@ package io.spine.internal.dependency
 @Suppress("MemberVisibilityCanBePrivate") // used directly from outside
 object Protobuf {
     private const val group = "com.google.protobuf"
-                    // Please keep the version `3.20.1` until this issue is resolved:
-    // https://github.com/protocolbuffers/protobuf-javascript/issues/127.
+                    /* Please keep the version `3.20.1` until this issue is resolved:
+       https://github.com/protocolbuffers/protobuf-javascript/issues/127. */
     const val version       = "3.20.1"
     val libs = listOf(
         "${group}:protobuf-java:${version}",
