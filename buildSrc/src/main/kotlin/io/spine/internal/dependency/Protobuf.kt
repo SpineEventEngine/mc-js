b5e34e5bf367cd73c54360f4af9d0abdb79ab9b4
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-16 11:35:13 +0000

    Fix comment language

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Protobuf.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Protobuf.kt
@@ -30,7 +30,7 @@ package io.spine.internal.dependency
 @Suppress("MemberVisibilityCanBePrivate") // used directly from outside
 object Protobuf {
     private const val group = "com.google.protobuf"
-                    // Please keep the version `3.20.1` until this issue is issue is resolved
+                    // Please keep the version `3.20.1` until this issue is resolved:
     // https://github.com/protocolbuffers/protobuf-javascript/issues/127.
     const val version       = "3.20.1"
     val libs = listOf(
